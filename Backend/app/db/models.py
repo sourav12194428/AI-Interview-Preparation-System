4428261e83514c9beaf2da72ec8115be719006b0
Rachita Sharma <rachita.sharma@tudip.com>
2026-04-03 15:57:42 +0530

    model file is created

--- Backend/app/db/models.py
+++ Backend/app/db/models.py
@@ -1,27 +1,71 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, Text, DateTime
-from sqlalchemy.orm import relationship, declarative_base
-import datetime
+from sqlalchemy import create_engine, Column, Enum, Integer, String, Text, text, ForeignKey, DateTime, Float, TIMESTAMP, func
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.db.base import Base   # your declarative base
 
-Base = declarative_base()
+# ---------------------------
+# 1. Interview Session
+# ---------------------------
+class Interview_Session(Base):
+        __tablename__ = "interview_session"
 
-class Session(Base):
-    __tablename__ = "sessions"
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        user_id = Column(Integer, nullable=False)
+        topic = Column(String, nullable=False)
+        tag = Column(Enum("easy", "medium", "hard", name="tag_enum"), nullable=False )
+        started_at = Column(TIMESTAMP,nullable=False, default=func.now())
+        completed_at = Column(TIMESTAMP,nullable=False, default=text("NOW() + INTERVAL '30 minutes'"))
+        status = Column(Enum("in_progress", "completed", name="status_enum"), default="in_progress")  # pending / in_progress / completed
+        overall_score = Column(Float, nullable=True)
+        final_feedback = Column(Text, nullable=True)
 
-    session_id = Column(String, primary_key=True, index=True)
-    topic = Column(String)
-    current_index = Column(Integer, default=0)
-    started_at = Column(DateTime, default=datetime.datetime.utcnow)
+        session_question=relationship("Session_Questions", back_populates="session")
+        answers=relationship("Session_Answers", back_populates="session")
+        
+# ---------------------------
+# 2. Session_Questions
+# ---------------------------
+class Session_Questions(Base):
+        __tablename__ = "session_questions"
 
-    questions = relationship("Question", back_populates="session")
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        session_id = Column(Integer, ForeignKey("interview_session.id"), nullable=False)
+        question_id = Column(Integer, ForeignKey("question_bank.id"), nullable=False)
 
-class Question(Base):
-    __tablename__ = "questions"
+        # Relationships
+        session = relationship("Interview_Session", back_populates="session_question")
+        question = relationship("Question_Bank", back_populates="session_question")
+        answers = relationship("Session_Answers", back_populates="session_question")
 
-    id = Column(Integer, primary_key=True, index=True)
-    session_id = Column(String, ForeignKey("sessions.session_id"))
-    text = Column(Text)
-    user_answer = Column(Text, nullable=True)
-    score = Column(Integer, nullable=True)
-    feedback = Column(Text, nullable=True)
+#---------------------------
+# 3. Session_Answers
+#---------------------------
+class Session_Answers(Base):
+        __tablename__ = "session_answers"
 
-    session = relationship("Session", back_populates="questions")
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        session_id = Column(Integer, ForeignKey("interview_session.id"), nullable=False)
+        session_question_id = Column(Integer, ForeignKey("session_questions.id"), nullable=False)
+        user_answer = Column(Text, nullable=True)
+        ai_feedback = Column(Text, nullable=True)
+        score = Column(Float, nullable=True)
+
+        # Relationships
+        session = relationship("Interview_Session",  back_populates="answers")
+        session_question = relationship("Session_Questions", back_populates="answers")
+
+
+#---------------------------
+# 4. Question_Bank
+#---------------------------
+class Question_Bank(Base):
+        __tablename__ = "question_bank"
+
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        question_text = Column(Text, nullable=False)
+        topic = Column(String, nullable=False)
+        tag = Column(Enum("easy", "medium", "hard", name="tag_enum"), nullable=True)
+        expected_answer = Column(Text, nullable=True)
+
+        session_question = relationship("Session_Questions", back_populates="question")
+
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
+from app.db.models import Interview_Session, Question_Bank, Session_Questions,Session_Answers
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
