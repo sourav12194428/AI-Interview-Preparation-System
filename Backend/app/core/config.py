cd1747e017e6c2ab7ce85c988c636f27ca7f3626
Rachita Sharma <rachita.sharma@tudip.com>
2026-04-06 14:11:09 +0530

    resolved 2

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -4,10 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()   # loads .env file
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-import os 
-
-from dotenv import load_dotenv 
-load_dotenv()   
-
-# loads .env file
-DATABASE_URL = os.getenv("DATABASE_URL")
